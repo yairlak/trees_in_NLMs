580959fd5c06e2f0b46e0760f2d61a53fbfe7ae9
Yair LAKRETZ <yl254115@is153802.intra.cea.fr>
2020-11-18 18:04:34 +0100

    plot word embeddings

--- code/plot_verb_embeddings.py
+++ code/plot_verb_embeddings.py
@@ -30,15 +30,6 @@ vocab = data.Dictionary(args.vocab)
 print(vocab)
 print(embeddings_in.shape)
 
-#### Plot verb embeddings
-pca = PCA(n_components=2)
-pca.fit(embeddings_in)
-print(pca.explained_variance_ratio_)
-V = pca.components_
-X_transformed = pca.fit_transform(embeddings_in)
-PC1 = V[0, :]
-PC2 = V[1, :]
-
 # Read list of contrasted words (e.g., singular vs. plural verbs).
 with open(args.input, 'r') as f:
     lines=f.readlines()
@@ -46,24 +37,32 @@ with open(args.input, 'r') as f:
     verbs_unaccusative = [l.strip() for l in lines[1].split(',')]
 print(verbs_transitive, verbs_unaccusative)
 
-fig, ax = plt.subplots(1, figsize = (35, 20))
-for i in tqdm(range(X_transformed.shape[0])):
-    word = vocab.idx2word[i]
-    if word in verbs_transitive:
-        color, size, alpha = 'b', 15, 1
-    elif word in verbs_unaccusative:
-        color, size, alpha = 'r', 15, 1
-    else:
-        color, size, alpha = 'k', 10, 0.2
+#### Plot verb embeddings
+for embeddings, embeddings_name in zip([embeddings_in, embeddings_out], ['embeddings_in', 'embeddings_out']):
+    pca = PCA(n_components=2)
+    pca.fit(embeddings)
+    print(pca.explained_variance_ratio_)
+    V = pca.components_
+    X_transformed = pca.fit_transform(embeddings)
 
-    ax.text(X_transformed[i, 0], X_transformed[i, 1], word, size=size, color=color, alpha=alpha)
-    lim_max = np.max(X_transformed)
-    lim_min = np.min(X_transformed)
-    ax.set_xlim((lim_min, lim_max))
-    ax.set_ylim((lim_min, lim_max))
-    ax.axis('off')
+    fig, ax = plt.subplots(1, figsize = (35, 20))
+    for i in tqdm(range(X_transformed.shape[0])):
+        word = vocab.idx2word[i]
+        if word in verbs_transitive:
+            color, size, alpha = 'b', 18, 1
+        elif word in verbs_unaccusative:
+            color, size, alpha = 'r', 18, 1
+        else:
+            color, size, alpha = 'k', 8, 0.1
 
-fn = os.path.join(args.path2save, 'word_embeddings.png')
-fig.savefig(fn)
-print('Saved to: ' + fn)
-plt.close(fig)
+        ax.text(X_transformed[i, 0], X_transformed[i, 1], word, size=size, color=color, alpha=alpha)
+        lim_max = np.max(X_transformed)
+        lim_min = np.min(X_transformed)
+        ax.set_xlim((lim_min, lim_max))
+        ax.set_ylim((lim_min, lim_max))
+        ax.axis('off')
+
+    fn = os.path.join(args.path2save, 'word_' + embeddings_name  + '.png')
+    fig.savefig(fn)
+    print('Saved to: ' + fn)
+    plt.close(fig)
